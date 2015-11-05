9aa90506ba48becb5603bcf36b955780db91d46e
iwat <chaiwats@truevisionstv.com>
2015-11-06 00:07:18 +0700

    Initial POC commit

--- SmartOrganizer/ViewController.swift
+++ SmartOrganizer/ViewController.swift
@@ -10,9 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var photoView: UIImageView?
+
+	// MARK: - ViewController
+
+	func cornersForImage(inputImage: UIImage) {
+		guard let photoView = photoView else {
+			return
+		}
+
+		let blendFilter = GPUImageAlphaBlendFilter()
+		blendFilter.forceProcessingAtSize(photoView.bounds.size)
+
+		let gammaFilter = GPUImageGammaFilter()
+		gammaFilter.addTarget(blendFilter)
+
+		let crosshairGenerator = GPUImageCrosshairGenerator()
+		crosshairGenerator.crosshairWidth = 15.0
+		crosshairGenerator.forceProcessingAtSize(photoView.bounds.size)
+		crosshairGenerator.addTarget(blendFilter)
+
+		let cornerFilter = GPUImageHarrisCornerDetectionFilter()
+		cornerFilter.threshold = 0.15
+		cornerFilter.forceProcessingAtSize(photoView.bounds.size)
+		cornerFilter.cornersDetectedBlock = { (cornersArray, numberOfCorners, frameTime) in
+			print("Got \(numberOfCorners) corner(s)")
+			for var i = UInt(0); i < numberOfCorners; i++ {
+				print("Corner[\(i)]: \(cornersArray[Int(i*2)])x\(cornersArray[Int(i*2+1)])")
+			}
+			crosshairGenerator.renderCrosshairsFromArray(cornersArray, count: numberOfCorners, frameTime: frameTime)
+		}
+
+		let stillImageSource = GPUImagePicture(image: inputImage)
+		stillImageSource.addTarget(cornerFilter)
+		stillImageSource.addTarget(gammaFilter)
+
+		blendFilter.useNextFrameForImageCapture()
+
+		stillImageSource.processImageWithCompletionHandler({
+			dispatch_async(dispatch_get_main_queue(), {
+				print("Finishing")
+				photoView.image = blendFilter.imageFromCurrentFramebuffer()
+			})
+		})
+	}
+
+	// MARK: - UIViewController
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		photoView?.backgroundColor = UIColor.cyanColor()
+		photoView?.image = UIImage(named: "test2")
+	}
+
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		cornersForImage(UIImage(named: "test2")!)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +73,4 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
 }
-
