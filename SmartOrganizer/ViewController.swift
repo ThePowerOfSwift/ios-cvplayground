6d27d9d8ebeeb8624c5c3f91cbe21742a9a69db5
iwat <chaiwats@truevisionstv.com>
2015-11-05 22:36:08 +0700

    Initial Commit

---
+++ SmartOrganizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartOrganizer
+//
+//  Created by iwat on 11/5/15.
+//  Copyright © 2015 AuthorWise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
